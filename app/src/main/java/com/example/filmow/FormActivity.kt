0a1d74d6697247cbc12198288ed1de046f30967d
Alamo <periclesn.0@gmail.com>
2019-11-08 01:42:58 -0300

    adc seekbar

--- app/src/main/java/com/example/filmow/FormActivity.kt
+++ app/src/main/java/com/example/filmow/FormActivity.kt
@@ -11,10 +11,12 @@ import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_form.*
+import org.w3c.dom.Text
 
 class FormActivity : AppCompatActivity() {
     private lateinit var etTitulo: EditText
-    private lateinit var etAvaliacao: EditText
+    private lateinit var sbAvaliacao: SeekBar
+    private lateinit var tvAvaliacao: TextView
     private lateinit var btSalvar: Button
     private lateinit var btCancelar: Button
 
@@ -23,7 +25,8 @@ class FormActivity : AppCompatActivity() {
         setContentView(R.layout.activity_form)
 
         this.etTitulo = findViewById(R.id.etFormTitulo)
-        this.etAvaliacao = findViewById(R.id.etFormAvaliacao)
+        this.sbAvaliacao = findViewById(R.id.sbFormAvaliacao)
+        this.tvAvaliacao = findViewById(R.id.tvFormAvaliacao)
         this.btSalvar = findViewById(R.id.btFormSalvar)
         this.btCancelar = findViewById(R.id.btFormCancelar)
 
@@ -32,12 +35,15 @@ class FormActivity : AppCompatActivity() {
             finish()
         })
 
+        this.sbAvaliacao.setOnSeekBarChangeListener(ChangeSeekBar())
+        this.tvAvaliacao.text = this.sbAvaliacao.progress.toString()
+
     }
 
     inner class OnClickBotao : View.OnClickListener{
         override fun onClick(v: View?) {
             val titulo = this@FormActivity.etTitulo.text.toString()
-            val avaliacao = this@FormActivity.etAvaliacao.text.toString()
+            val avaliacao = this@FormActivity.tvAvaliacao.text.toString()
             val filme = Filme(titulo, avaliacao)
             val itPessoa = Intent()
 
@@ -45,6 +51,19 @@ class FormActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK, itPessoa)
             finish()
         }
+    }
+
+    inner class ChangeSeekBar : SeekBar.OnSeekBarChangeListener{
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            this@FormActivity.tvAvaliacao.text = progress.toString()
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        }
+
     }
 
 }
